bfa213b21ab585432b9f4c05178b1ac04cd26730
V. Karras <v.karras98@gmail.com>
2025-01-28 16:58:01 +0200

    World changed to CF in println

--- src/gr/aueb/cf/ch1/HelloWorld2.java
+++ src/gr/aueb/cf/ch1/HelloWorld2.java
@@ -3,6 +3,6 @@ package gr.aueb.cf.ch1;
 public class HelloWorld2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello CF");
     }
 }
